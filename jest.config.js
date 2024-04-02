9e796e75871e7a0917f304713d9325004372d9de
Mike Storey <mike@agile-learning.institute>
2024-04-02 19:13:51 -0400

    Jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    roots: ['<rootDir>/tests'],
+    roots: ['<rootDir>/test'],
     testMatch: ['**/*.test.ts'],
     collectCoverage: true,
     coverageDirectory: 'coverage',
