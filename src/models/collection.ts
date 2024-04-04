e4143e07c8db688fb3f0161441a713cdee621ef6
Mike Storey <mike@agile-learning.institute>
2024-04-03 21:21:00 -0400

    Improved collection testing

--- src/models/collection.ts
+++ src/models/collection.ts
@@ -1,40 +1,52 @@
 import { Config } from "../config/config";
 import { Version } from "./version"; // Now importing Version from a separate module
-import { readFileSync } from "fs";
-import { join } from "path";
 
+interface CollectionConfig {
+    collectionName: string;
+    versions: any[];
+}
+
+/**
+ * This class processes a collection configuration file
+ */
 export class Collection {
     private config: Config;
     private collectionName: string;
     private versions: Version[] = [];
-    private currentVersion: string;
+    private currentVersion: string = "";
 
-    constructor(theConfig: Config, configFile: string) {
+    constructor(theConfig: Config, collectionConfig: CollectionConfig) {
         this.config = theConfig;
-        const filePath = join(this.config.getConfigFolder(), configFile);
-        const configFileContents = readFileSync(filePath, 'utf8');
-        const configData = JSON.parse(configFileContents);
-
-        this.collectionName = configData.name;
-        this.versions = configData.versions;
-        this.currentVersion = this.getVersion();
+        this.collectionName = collectionConfig.collectionName;
+        collectionConfig.versions.forEach(version => {
+            this.versions.push(new Version(this.config, this.collectionName, version));
+        });
     }
 
-    public async processVersions(): void {
-        const db = this.config.getDatabase();
-        const collection = db.collection(this.collectionName);
-        
+    public async processVersions() {
+        this.currentVersion = await this.getVersion();
         for (const version of this.versions) {
             if (version.getVersion() > this.currentVersion) {
-                await version.apply(config.getDatabase());
-                this.currentVersion = this.getVersion(); 
+                version.apply();
+                this.currentVersion = await this.getVersion();
             }
         }
     }
 
-    public getVersion(): string {
-        const db = this.config.getDatabase();
-        const versionDocument = db.collection(this.collectionName).findOne({ name: "VERSION" });
+    public async getVersion(): Promise<string> {
+        const versionDocument = await this.config.getCollection(this.collectionName).findOne({ name: "VERSION" });
         return versionDocument ? versionDocument.version : "0.0.0";
+    }
+
+    public getCurrentVersion(): string {
+        return this.currentVersion;
+    }
+    
+    public getName(): string {
+        return this.collectionName;
+    }
+
+    public getVersions(): Version[] {
+        return this.versions;
     }
 }
--- test/models/collection.test.ts
+++ test/models/collection.test.ts
@@ -1,18 +1,64 @@
+import { Config } from '../../src/config/config';
 import { Collection } from '../../src/models/collection';
 
+// Setup for mocking MongoDB findOne
+const findOneMock = jest.fn();
+
+// Mock the Version class
+jest.mock("../../src/models/version", () => {
+    return {
+        Version: jest.fn().mockImplementation(() => {
+            return {
+                getVersion: jest.fn().mockReturnValue("1.0.0"),
+                apply: jest.fn()
+            };
+        })
+    };
+});
+
+// Mock the Config
+jest.mock("../../src/config/config", () => {
+    return {
+        Config: jest.fn().mockImplementation(() => {
+            return {
+                getCollection: jest.fn().mockReturnValue({
+                    findOne: findOneMock
+                })
+            };
+        })
+    };
+});
+
 describe('Collection', () => {
-    let collection: Collection;
 
-    beforeAll( () => {
-    });
+    test('test constructor', () => {
+        process.env.CONFIG_FOLDER = "./test/resources";
+        const config = new Config();
+        const collectionConfig = {
+            "collectionName": "foo",
+            "versions": [{ "version": "1.0.0" }]
+        };
+        const theCollection = new Collection(config, collectionConfig);
 
-    afterAll(async () => {
-    });
-
-    test('test constructor', async () => {
+        expect(theCollection.getName()).toBe("foo");
+        expect(theCollection.getVersions().length).toBe(1);
+        expect(theCollection.getVersions()[0].getVersion()).toBe("1.0.0");
     });
 
     test('test process', async () => {
+        // Mock the response of findOne to simulate getting the current version from the database
+        findOneMock.mockResolvedValue({ version: "0.9.0" });
+
+        const config = new Config();
+        const collectionConfig = {
+            collectionName: "testCollection",
+            versions: [{ version: "1.0.0" }]
+        };
+
+        const collection = new Collection(config, collectionConfig);
+        expect(collection.getCurrentVersion()).toBe("");
+        await collection.processVersions();
+        expect(collection.getCurrentVersion()).toBe("0.9.0");
     });
 
 });
