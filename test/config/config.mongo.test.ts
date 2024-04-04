a38779c9d876d1bfc5300c4899751b596cd8745f
Mike Storey <mike@agile-learning.institute>
2024-04-04 16:38:57 -0400

    framed in testing - mongodb calls still remain

--- test/config/config.mongo.test.ts
+++ test/config/config.mongo.test.ts
@@ -1,68 +1,106 @@
 /**
  * NOTE: This set of unit tests requires a mongodb 
- * You can run a monog container with the following command
+ * You can run a mongo container with the following command
  * docker run -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=example --detach mongo:latest
  */
 import { Config } from '../../src/config/Config';
 
 describe('Config', () => {
     let config: Config;
+    let collectionName = "testCollection";
 
-    // Clear all mocks before each test
-    beforeEach(() => {
+    beforeEach(async () => {
         config = new Config();
+        await config.connect();
     });
 
-    test('test connect/disconnect', async () => {
-        expect(() => config.getCollection("foo")).toThrow("Database not connected");
-
-        await config.connect();
-        let collection = config.getCollection("foo");
-        expect(collection.collectionName).toBe("foo");
-
-        await config.disconnect();
-        expect(() => config.getCollection("foo")).toThrow("Database not connected");
+    afterEach(async () => {
+        await config.disconnect()
     });
 
-    test('test getDatabase', async () => {
-        expect(() => config.getDatabase()).toThrow("Database not connected");
-
-        await config.connect();
-        let db = config.getDatabase();
-        expect(db.databaseName).toBe("test");
-
-        await config.disconnect();
-        expect(() => config.getDatabase()).toThrow("Database not connected");
+    afterAll(async () => {
+        // config.getDatabase.DROP COLLECTION collectionName
     });
 
     test('test getCollection', async () => {
-        expect(() => config.getCollection("foo")).toThrow("Database not connected");
+        let collection = config.getCollection(collectionName);
+        expect(collection.collectionName).toBe(collectionName);
+    });
 
-        await config.connect();
-        let collection = config.getCollection("foo");
-        expect(collection.collectionName).toBe("foo");
-
-        await config.disconnect();
-        expect(() => config.getCollection("foo")).toThrow("Database not connected");
+    test('test getDatabase', async () => {
+        let db = config.getDatabase();
+        expect(db.databaseName).toBe("test");
     });
 
     test('test set/getVersion', async () => {
-        // TODO
+        let theVersion = await config.getVersion(collectionName);
+        expect(theVersion).toBe("0.0.0");
+
+        await config.setVersion(collectionName, "1.2.3");
+        theVersion = await config.getVersion(collectionName);
+        expect(theVersion).toBe("1.2.3");
+
+        await config.setVersion(collectionName, "2.2.2");
+        theVersion = await config.getVersion(collectionName);
+        expect(theVersion).toBe("2.2.2");
     });
 
     test('test apply/remove schema validation', async () => {
-        // TODO
+        let applyedSchema = {};
+        let schema = {
+            "bsonType": "object",
+            "properties": {
+                "name": {
+                    "description": "Name Description",
+                    "bsonType": "string",
+                }
+            }
+        };
+
+        config.applySchemaValidation(collectionName, schema);
+        applyedSchema = {}; // mongodb get schema
+        expect(applyedSchema).toStrictEqual(schema);
+
+        config.clearSchemaValidation(collectionName);
+        applyedSchema = {}; // mongodb get schema
+        expect(applyedSchema).toStrictEqual({});
     });
 
     test('test add/drop indexes', async () => {
-        // TODO
+        let appliedIndexes = [];
+        let indexes = [
+
+        ];
+        let names = [
+
+        ];
+
+        config.addIndexes(indexes);
+        appliedIndexes = []; // mongodb get indexes
+        expect(appliedIndexes).toStrictEqual(indexes);
+
+        config.dropIndexes(names);
+        appliedIndexes = []; // mongodb get indexes
+        expect(appliedIndexes).toStrictEqual([]);
     });
 
     test('test aexecuteAggregations', async () => {
-        // TODO
+        const aggregations = {
+
+        };
+        const expectedOutput = {
+
+        };
+
+        config.executeAggregations(aggregations);
+        const result = {}; // mongodb get results
+        expect(result).toStrictEqual(expectedOutput);
     });
 
     test('test bulkLoad', async () => {
-        // TODO
+        const testData = [];
+        config.bulkLoad(collectionName, testData);
+        const result = []; //mongodb get all
+        expect(result).toStrictEqual(testData);
     });
 });
